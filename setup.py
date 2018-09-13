b79ae61afa69144db83c4c11a5af5eccb47c8858
Galen Curwen-McAdams <galencm@fastmail.com>
2018-09-13 03:17:43 +0000

    Problem: linting/formatting are a single commit
    Solution: revert and use two commits for linting/formatting
    
    Revert "Problem: code has not been formatted or linted"
    
    This reverts commit 32eccba4074f15d703af6157e39eecb3026d4a69.

--- setup.py
+++ setup.py
@@ -1,23 +1,20 @@
 from setuptools import find_packages, setup
-
 setup(
-    name="machinic_bespoke",
+name="machinic_bespoke",
     version="0.1",
     description="",
     author="Galen Curwen-McAdams",
-    author_email="",
+    author_email='',
     platforms=["any"],
     license="Mozilla Public License 2.0 (MPL 2.0)",
     include_package_data=True,
-    data_files=[("", ["LICENSE.txt"])],
+    data_files = [("", ["LICENSE.txt"])],
     url="",
     packages=find_packages(),
-    install_requires=["redis", "pre-commit"],
+    install_requires=['redis', 'pre-commit'],
     dependency_links=[],
-    entry_points={
-        "console_scripts": [
-            "bespoke-animate = machinic_bespoke.animative:main",
-            "bespoke-doc = machinic_bespoke.bespoke_doc:main",
-        ]
-    },
+    entry_points = {'console_scripts': ['bespoke-animate = machinic_bespoke.animative:main',
+                                        'bespoke-doc = machinic_bespoke.bespoke_doc:main'
+                                       ],
+                            },
 )
